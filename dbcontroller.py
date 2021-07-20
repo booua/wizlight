644ce3a5ef3169c5213532fc4433d690a3a1a100
Jacek Paciorek <jack.paciorek@gmail.com>
2021-07-20 18:27:43 +0200

    catch errors

--- dbcontroller.py
+++ dbcontroller.py
@@ -1,27 +1,38 @@
 import sqlite3
 import time
 
+
 def dbInit():
-    con = sqlite3.connect('bulbs.db')
-    cur = con.cursor()
-    cur.execute("DROP TABLE bulbs")
-    cur.execute('''CREATE TABLE IF NOT EXISTS bulbs
-                (date text, ip_address text)''')
-    con.commit()
-    con.close()
+    try:
+        con = sqlite3.connect('bulbs.db')
+        cur = con.cursor()
+        cur.execute("DROP TABLE bulbs")
+        cur.execute('''CREATE TABLE IF NOT EXISTS bulbs
+                    (date text, ip_address text)''')
+        con.commit()
+        con.close()
+    except sqlite3.Error as e:
+        print(e)
+   
 
 def saveIpAddress(ip_address):
-    con = sqlite3.connect('bulbs.db')
-    cur = con.cursor()
-    cur.execute("insert into bulbs values (?, ?)", (time.time(), ip_address))
-    con.commit()
-    con.close()
+    try:
+        con = sqlite3.connect('bulbs.db')
+        cur = con.cursor()
+        cur.execute("insert into bulbs values (?, ?)", (time.time(), ip_address))
+        con.commit()
+        con.close()
+    except sqlite3.Error as e:
+        print(e)
 
 def getIpAddress():
-    con = sqlite3.connect('bulbs.db')
-    cur = con.cursor()
-    cur.execute("select ip_address from bulbs")
-    ip_address = cur.fetchone()
-    con.commit()
-    con.close()
+    try:
+        con = sqlite3.connect('bulbs.db')
+        cur = con.cursor()
+        cur.execute("select ip_address from bulbs")
+        ip_address = cur.fetchone()
+        con.commit()
+        con.close()
+    except sqlite3.Error as e:
+        print(e)
     return ip_address[0]
